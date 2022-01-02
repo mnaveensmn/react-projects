b18177514eef401bd8a73aa424cb42ddecaf93e1
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-02 23:12:11 +0530

    redux:create redux advanced
    Created the book cart application using redux existing knowledge like adding the book to cart, updating the existing cart and updating the quantiy and amount details.

--- 18-redux-advanced/src/components/Cart/Cart.js
+++ 18-redux-advanced/src/components/Cart/Cart.js
@@ -1,15 +1,28 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
+import { useSelector } from "react-redux";
 
 const Cart = (props) => {
+
+  const CartItems = useSelector(state=> state.cart.items)
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {CartItems.map((product) => (
+          <CartItem
+            key={product.id}
+            item={{
+              id: product.id,
+              price: product.price,
+              quantity: product.quantity,
+              total: product.totalPrice,
+              title: product.name,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
