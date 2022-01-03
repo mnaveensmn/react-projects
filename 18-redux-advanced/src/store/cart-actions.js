efd28d35e5fdfc50a809ecffc46d75cc3cf6d7a0
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-03 23:01:44 +0530

    redux: work with asynchronous task
    Worked with asynchronous task in redux. Fetched the cart data from firebase.
    Pushed the cart data to firebase.Worked on thunk functions.

---
+++ 18-redux-advanced/src/store/cart-actions.js
@@ -0,0 +1,67 @@
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-http-5c577-default-rtdb.firebaseio.com/cart.json"
+      );
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+      const data = await response.json();
+      return data;
+    };
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+      uiActions.showNotification({
+        status: "success",
+        title: "Success!",
+        message: "Fetched cart data!",
+      });
+    } catch (error) {}
+  };
+};
+
+export const sendCartData = (cartData) => {
+    return async (dispatch)=> {
+        dispatch(
+          uiActions.showNotification({
+            status: "Pending",
+            title: "Sending...",
+            message: "Sending cart data!",
+          })
+        );
+        const sendRequest = async () => {
+          const response = await fetch(
+            "https://react-http-5c577-default-rtdb.firebaseio.com/cart.json",
+            { method: "PUT", body: JSON.stringify(cartData) }
+          );
+
+          if (!response.ok) {
+            throw new Error();
+          }
+        };
+        try {
+          await sendRequest();
+          dispatch(
+            uiActions.showNotification({
+              status: "success",
+              title: "Success!",
+              message: "Sent cart data successfull!",
+            })
+          );
+        } catch (error) {
+          dispatch(
+            uiActions.showNotification({
+              status: "error",
+              title: "Error!",
+              message: "Sent cart data failed!",
+            })
+          );
+        }
+        
+    };
+};
