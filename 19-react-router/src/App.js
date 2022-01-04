f3754e2e19b54c2e5019ec5cd97305f4722fa56f
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-04 21:58:55 +0530

    router: fixed the issue on page file path.

--- 19-react-router/src/App.js
+++ 19-react-router/src/App.js
@@ -1,16 +1,16 @@
 
 import { Route } from "react-router-dom";
-import Products from "./components/Products";
-import Welcome from "./components/Welcome";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
       <Route path="/welcome">
-        <Welcome />
+        <Welcome/>
       </Route>
       <Route path="/products">
-        <Products />
+        <Products/>
       </Route>
     </div>
   );
