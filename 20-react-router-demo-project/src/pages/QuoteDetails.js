63225761a454684c88ccaf8f4289eb00171d06d0
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-08 18:50:33 +0530

    router: implement Quote app functionality
    Added the functionality for fetching and pushing quotes to firebase.
    Worked on functionality for loading and adding the comments.

--- 20-react-router-demo-project/src/pages/QuoteDetails.js
+++ 20-react-router-demo-project/src/pages/QuoteDetails.js
@@ -1,30 +1,55 @@
-import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Fragment, useEffect } from "react";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-    {id: 'q1', author:'Max', text:'Learning React is fun!'},
-    {id: 'q2', author:'Naveen', text:'Express your learning through blog'},
-    {id: 'q3', author:'Naveen', text:'Contribute to community'},
-    {id: 'q4', author:'Naveen', text:'Learn every single day !'},
-];
-
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetail = () => {
-  
+    const match = useRouteMatch();
     const params = useParams();
+    const { quoteId } = params;
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+    const {
+      sendRequest,
+      status,
+      data: loadedQuote,
+      error,
+    } = useHttp(getSingleQuote, true);
 
-    if (!quote) {
-        return <p>No quote found</p>
+   
+    useEffect(() => {
+      sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    if (status === 'pending') {
+        return (
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        );
     }
 
+    if (error) {
+        return <p className="centered">{error}</p>
+    }
+
+    if (!loadedQuote) {
+        return <p>No quote found</p>
+    }
+    console.log(loadedQuote);
     return (
       <Fragment>
-        <HighlightedQuote text={quote.text} author={quote.author} />
-        <Route path="/quotes/:quoteId/comments">
+        <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+        <Route path={match.path} exact>
+          <div className="centered">
+            <Link className="btn--flat" to={`${match.url}/comments`}>
+              Load Comments
+            </Link>
+          </div>
+        </Route>
+        <Route path={`${match.path}/comments`}>
           <Comments />
         </Route>
       </Fragment>
