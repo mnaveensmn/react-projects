d3bce8f4b1e7d7181dd96a77b58d4badfd6e4650
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-16 20:02:47 +0530

    animation: Worked on Transition and its feature.

--- 24-react-animations/src/components/Modal/Modal.js
+++ 24-react-animations/src/components/Modal/Modal.js
@@ -1,19 +1,40 @@
 import React from 'react';
-
+import Transition from 'react-transition-group/Transition';
 import './Modal.css';
 
-const Modal = (props) =>{
+const Modal = (props) => {
+  const animationTiming = {
+    enter: 400,
+    exit: 1000
+  }
+  return (
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
 
-    const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClose"];
-    
-    return (
-      <div className={cssClasses.join(" ")}>
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>
-          Dismiss
-        </button>
-      </div>
-    );
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
+  );
 };
 
 export default Modal;
