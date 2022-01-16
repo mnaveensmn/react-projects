83c64fcb741caa9bfe65df9891a38b52d0d61e5c
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-16 20:02:47 +0530

    animation: Worked on Transition and its feature.

--- 24-react-animations/src/App.js
+++ 24-react-animations/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from 'react-transition-group/Transition';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -8,7 +8,8 @@ import List from "./components/List/List";
 class App extends Component {
 
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = ()=> {
@@ -27,8 +28,42 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() => {
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }));
+          }}
+        >
+          Toogle
+        </button>
+        <Transition
+          mountOnEnter
+          unmountOnExit
+          in={this.state.showBlock}
+          timeout={300}
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExit={() => console.log("onExit")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExited")}
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        <br />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
