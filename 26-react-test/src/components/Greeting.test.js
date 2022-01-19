3e56c058f6b0054eea681c495b3c488fb0a4b850
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-19 23:47:45 +0530

    react-test: Add test
    Created a component and test the same. Written the test for user interaction and state.

---
+++ 26-react-test/src/components/Greeting.test.js
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greatings";
+import userEvent from '@testing-library/user-event'; 
+
+describe('Greeting component', () => {
+    test("renders Hello World as a test", () => {
+      //Arrange
+      render(<Greeting />);
+
+      //Act
+      //... nothing
+
+      //Assert
+      const holloWorldElement = screen.getByText("Hello World!", {
+        exact: false,
+      });
+      expect(holloWorldElement).toBeInTheDocument();
+    });
+
+    test('renders "good to see" you if the button was not clicked',()=>{
+        render(<Greeting/>);
+        const outputElement = screen.getByText("good to see you", {
+          exact: false,
+        });
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('renders "Changed!" if the button was clicked',()=>{
+        //Arrange
+        render(<Greeting/>);
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement)
+
+        //Assert
+        const outputElement = screen.getByText("Changed!");
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('should not renders "good to see" when "Changed!" had been rendered',()=>{
+        //Arrange
+        render(<Greeting/>);
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement)
+
+        //Assert
+        const outputElement = screen.queryByText("good to see you", {
+          exact: false,
+        });
+        expect(outputElement).toBeNull();
+    });
+    
+});
+
