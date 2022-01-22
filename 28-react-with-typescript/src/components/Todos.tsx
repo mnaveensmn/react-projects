ddb7efe2c715f0208895db5c71e08a33f8cda999
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-22 16:34:33 +0530

    react-typescript: worked with components and props in typescript.

---
+++ 28-react-with-typescript/src/components/Todos.tsx
@@ -0,0 +1,15 @@
+
+type TodoData = {
+    items: string[]
+}
+const Todos: React.FC<TodoData> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
