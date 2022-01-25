d55d76619f870b8406e11e05c79c25b81565cc71
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-01-25 14:06:07 +0530

    react-hook: Added the useState for IngredientForm.

--- 29-react-hooks/src/components/Ingredients/IngredientForm.js
+++ 29-react-hooks/src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  
+  const [input,setInput] = useState({ title: "", amount: "" });
+
+  console.log(input);
+
+  const titleChangeHandler = (event) => {
+    setInput({
+      title: event.target.value,
+      amount: input.amount
+    });
+  }
+
+  const amountChangeHandler = (event) => {
+    setInput({
+      title: input.title,
+      amount: event.target.value,
+    });
+  }
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +34,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={input.title} onChange={titleChangeHandler}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={input.amount} onChange={amountChangeHandler}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
