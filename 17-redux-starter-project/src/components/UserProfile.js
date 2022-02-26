b50d683ff5cf92165e74e85801f46718fa24d85e
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-02-26 23:58:06 +0530

    refactor

--- 17-redux-starter-project/src/components/UserProfile.js
+++ 17-redux-starter-project/src/components/UserProfile.js
@@ -3,7 +3,7 @@ import classes from './UserProfile.module.css';
 const UserProfile = () => {
   return (
     <main className={classes.profile}>
-      <h2>My User Profile</h2>
+      <h2>User Profile</h2>
     </main>
   );
 };
